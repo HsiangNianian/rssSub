6651ace8c6983efc84f2af411099d485a22865a1
Hsiang Nianian <Hsiangnianian@outlook.com>
2023-01-08 21:33:30 +0800

    Add files via upload

---
+++ script/Constant.lua
@@ -0,0 +1,33 @@
+local modname = "CONST"
+local M = {}
+_G[modname] = M
+package.loaded[modname] = M
+local DIR_SEP = package.config:sub(1,1)
+M.INFO = {
+    VERSION = 'release-0.1.4',
+    MOD_NAME = 'rssSub',
+    AUTHOR = '简律纯',
+    LICENSE = 'MIT'
+};
+M.DIR_LIST = {}
+M.DIR_LIST.MOD = getDiceDir()..DIR_SEP.."mod"..DIR_SEP..M.INFO.MOD_NAME..DIR_SEP
+M.DIR_LIST.MOD_SCRIPT = M.DIR_LIST.MOD.."script"..DIR_SEP
+M.CONTROL = M.DIR_LIST.MOD_SCRIPT.."control"..DIR_SEP
+M.XML2LUA = M.DIR_LIST.MOD_SCRIPT.."xml2lua"..DIR_SEP
+M.DEFAULT_CONFIG = [[_C = {
+    target = {
+        user_id = {
+        },
+        group_id = {
+            
+        }
+    },
+    feeds = {
+        native_src = {
+        
+        },
+        online_src = {
+            
+        }
+    }
+}]]
